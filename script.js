f41bfcdba175e538b42d7e3cccff6727cc4c5214
renataap13 <renataap_1810@hotmail.com>
2023-06-27 16:03:20 -0300

    formulario 26/07

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+document.getElementById('enviar').addEventListener('click',function(event) {
+    event.preventDefault()
+function event(){
+    let email = document.getElementById("email").value ;
+    let senha = document.getElementById("senha").value ;
+   console.log('nome:',nome);
+   console.log('Email:',email);
+}
+});
